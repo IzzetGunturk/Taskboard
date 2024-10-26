e3c063ba0792879c0da5bbd4ff8aa05a8905cc65
IzzetGunturk <izzetgunturk2@hotmail.com>
2024-10-27 01:02:29 +0200

    Added changing title and text

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -17,6 +17,11 @@ export default {
       newInProgressTasks: '',
       newInReviewTasks: '',
       newDoneTasks: '',
+
+      todoTitle: '📝 To do',
+      inProgressTitle: '🔨 In progress',
+      inReviewTitle: '🧐 In review',
+      doneTitle: '✅ Done',
     };
   },
   methods: {
@@ -27,19 +32,29 @@ export default {
       else {
         alert('Fill in a task...')
       }
-    }
+    },
+
+    deleteTask(arrayTask, index) {
+      arrayTask.splice(index, 1);
+    },
   }
 };
 </script>
 
 <template>
   <div class="container">
+
     <div class="column">
-      <p>📝 To do</p>
+      <p contenteditable="true" @blur="updateTitle('todoTitle', $event.target.innerText)">{{ todoTitle }}</p>
       <draggable v-model="todoTasks" group="tasks" class="tasks">
         <template #item="{ element: task }">
           <div class="task">
-            <p>{{ task }}</p>
+            <div class="iconContainer">
+              <svg class="icon" @click="deleteTask(todoTasks, index)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+              </svg>
+            </div>
+            <p class="taskText" contenteditable="true" @blur="updateTitle($event.target.innerText)">{{ task }}</p>
           </div>
         </template>
       </draggable>
@@ -51,11 +66,16 @@ export default {
     </div>
     
     <div class="column">
-      <p>🔨 In progress</p>
+      <p contenteditable="true" @blur="updateTitle('inProgressTitle', $event.target.innerText)">{{ inProgressTitle }}</p>
       <draggable v-model="inProgressTasks" group="tasks" class="tasks">
         <template #item="{ element: task }">
           <div class="task">
-            <p>{{ task }}</p>
+            <div class="iconContainer">
+              <svg class="icon" @click="deleteTask(inProgressTasks, index)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+              </svg>
+            </div>
+            <p class="taskText" contenteditable="true" @blur="updateTitle($event.target.innerText)">{{ task }}</p>
           </div>
         </template>
       </draggable>
@@ -67,11 +87,16 @@ export default {
     </div>
     
     <div class="column">
-      <p>🧐 In review</p>
+      <p contenteditable="true" @blur="updateTitle('inReviewTitle', $event.target.innerText)">{{ inReviewTitle }}</p>
       <draggable v-model="inReviewTasks" group="tasks" class="tasks">
         <template #item="{ element: task }">
           <div class="task">
-            <p>{{ task }}</p>
+            <div class="iconContainer">
+              <svg class="icon" @click="deleteTask(inReviewTasks, index)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+              </svg>
+            </div>
+            <p class="taskText" contenteditable="true" @blur="updateTitle($event.target.innerText)">{{ task }}</p>
           </div>
         </template>
       </draggable>
@@ -79,16 +104,20 @@ export default {
       <div class="addTask">
         <textarea class="dynamicTextarea" v-model="newInReviewTasks" placeholder="Fill in a task..."></textarea>
         <button class="addTaskButton" @click="addTask(inReviewTasks, newInReviewTasks); newInReviewTasks = ''">+ Add a task</button>
-        
       </div>
     </div>
     
     <div class="column">
-      <p>✅ Done</p>
+      <p contenteditable="true" @blur="updateTitle('doneTitle', $event.target.innerText)">{{ doneTitle }}</p>
       <draggable v-model="doneTasks" group="tasks" class="tasks">
         <template #item="{ element: task }">
           <div class="task">
-            <p>{{ task }}</p>
+            <div class="iconContainer">
+              <svg class="icon" @click="deleteTask(doneTasks, index)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+              </svg>
+            </div>
+            <p class="taskText" contenteditable="true" @blur="updateTitle($event.target.innerText)">{{ task }}</p>
           </div>
         </template>
       </draggable>
@@ -138,11 +167,29 @@ export default {
   border-radius: 10px;
   padding: 10px;
   cursor: move;
+  display: flex;
+  flex-direction: column;
 }
 
-.task p {
-  padding: 10px;
+.taskText {
+  padding: 5px;
   margin: 0;
+}
+
+.iconContainer {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.icon {
+  width: 20px;
+  cursor: pointer;
+  color: #b6c2cf5d;
+}
+
+.icon:hover {
+  color: #b6c2cf;
+  transition-duration: 200ms;
 }
 
 .dynamicTextarea {
