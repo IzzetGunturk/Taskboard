1bb570fcf37a6f8eb46a848532624b2ccefa6a78
IzzetGunturk <izzetgunturk2@hotmail.com>
2024-11-17 19:35:27 +0100

    Added functions for adding table and deleting table

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -12,7 +12,7 @@ export default {
         { title: '📝 To do', tasks: [], newTask: '' },
         { title: '🔨 In progress', tasks: [], newTask: '' },
         { title: '🧐 In review', tasks: [], newTask: '' },
-        { title: '✅ Done', tasks: [], newTask: '' }
+        { title: '✅ Done', tasks: [], newTask: '' },
       ]
     };
   },
@@ -32,15 +32,40 @@ export default {
       const column = this.columns[columnIndex] // finding the colom
       column.tasks.splice(taskIndex, 1);
     },
+
+    addTable() {
+      this.columns.push([])
+    },
+
+    deleteTable(index) {
+      this.columns.splice(index, 1)
+    }
+    
   }
 };
 </script>
 
 <template>
+  <div class="addTableContainer">
+    <div class="buttonAddTable" @click="addTable">
+      <p>Add table</p>
+      <svg class="addIcon" mlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960">
+        <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/>
+      </svg>
+    </div>
+  </div>
   <div class="container">
-
     <div v-for="(column, index) in columns" :key="index" class="column">
-      <p contenteditable="true" @blur="updateTitle($event.target.innerText)">{{ column.title }}</p>
+      <div class="titleSection">
+        <p contenteditable="true" @blur="updateTitle($event.target.innerText)">
+          {{ column.title || 'Fill in a title...' }}
+        </p>
+        <div>
+          <svg class="icon" @click="deleteTable(index)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+          </svg>
+        </div>
+      </div>
       <draggable v-model="column.tasks" group="tasks" class="tasks">
         <template #item="{ element: task }">
           <div class="task">
@@ -59,10 +84,40 @@ export default {
         <button class="addTaskButton" @click="addTask(index);">+ Add a task</button>
       </div>
     </div>
-  </div>
+</div>
 </template>
 
 <style>
+.addTableContainer {
+  display: flex;
+  justify-content: flex-end;
+  padding-left: 30px;
+  padding-right: 30px;
+}
+
+.buttonAddTable {
+  background-color: #101204;
+  padding: 10px 12px;
+  border-radius: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 30px;
+  gap: 6px;
+  color: #fff;
+  font-size: 13px;
+  cursor: pointer;
+}
+
+.buttonAddTable:hover {
+  transition-duration: 200ms;
+  background-color: #2f2f2f;
+}
+
+.addIcon {
+  fill: #fff;
+}
+
 .container {
   display: flex;
   flex-direction: row;
@@ -70,6 +125,12 @@ export default {
   justify-content: center;
   gap: 30px;
   padding: 30px;
+}
+
+.titleSection {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 }
 
 .column {
